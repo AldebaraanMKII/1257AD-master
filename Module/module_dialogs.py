f9c1729dc56314eac109ebb44507255cdbd9c3ec
AldebaraanMKII <21088681+AldebaraanMKII@users.noreply.github.com>
2019-05-05 14:13:25 -0300

    Update module_dialogs.py
    fixed compilation error

--- Module/module_dialogs.py
+++ Module/module_dialogs.py
@@ -11731,7 +11731,7 @@ What kind of recruits do you want?", "dplmc_constable_recruit_select",
     (try_end),
 	########### NEW v2.9 - improved relations with pope after crusade
     (store_random_in_range, ":random", 5, 8),
-    (call_script, "script_change_player_relation_with_troop", ":trp_pope", ":random"),
+    (call_script, "script_change_player_relation_with_troop", "trp_pope", ":random"),
 	###########
     (str_store_faction_name, s1, "$crusade_target_faction"),
  ], "So be it, I'll declare the crusade against {s1}. Deus Vult!", "lord_talk",[]],   
